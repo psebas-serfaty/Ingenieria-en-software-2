9fa80705e308a3933105f737ae03ee72fe71859e
Pablo Sebastian Serfaty <psebaspier@hotmail.com>
2023-03-12 20:31:05 -0300

    acomode cosas

--- obj/Archivo.cs
+++ obj/Archivo.cs
@@ -5,8 +5,19 @@ using System.Threading.Tasks;
 
 namespace Ingenieria_en_software_2.obj
 {
-    public class Archivo
+    public class Archivo : ElementoBase
+{
+    private string extension;
+
+    public Archivo(string nombre, double peso, string extension) : base(nombre, peso)
     {
-        
+        this.extension = extension;
     }
+
+    public string Extension
+    {
+        get { return extension; }
+        set { extension = value; }
+    }
+}
 }
--- obj/Carpeta.cs
+++ obj/Carpeta.cs
@@ -5,8 +5,23 @@ using System.Threading.Tasks;
 
 namespace Ingenieria_en_software_2.obj
 {
-    public class carpeta
+    public class Carpeta : ElementoBase
+{
+    private List<ElementoBase> elementos;
+
+    public Carpeta(string nombre, double peso) : base(nombre, peso)
     {
-        
+        elementos = new List<ElementoBase>();
     }
+
+    public void AgregarElemento(ElementoBase elemento)
+    {
+        elementos.Add(elemento);
+    }
+
+    public void AgregarElemento<T>(T elemento) where T : ElementoBase
+    {
+        elementos.Add(elemento);
+    }
+}
 }
