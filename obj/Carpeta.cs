cc347344e2aedb47849ed2b6de8663862e1b7309
Pablo Sebastian Serfaty <psebaspier@hotmail.com>
2023-03-12 19:50:07 -0300

    cositas

---
+++ obj/Carpeta.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ingenieria_en_software_2.obj
+{
+    public class carpeta
+    {
+        
+    }
+}
