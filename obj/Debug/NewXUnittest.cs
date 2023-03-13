673c5dcc1be1060b5d0110dc58c7f19571e6e372
Pablo Sebastian Serfaty <psebaspier@hotmail.com>
2023-03-12 21:06:01 -0300

    tratando de hacer andar los test

---
+++ obj/Debug/NewXUnittest.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Ingenieria_en_software_2.obj.Debug
+{
+    public class NewXUnittest
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
